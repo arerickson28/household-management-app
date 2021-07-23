9855b6a717792c814cc818d0caf878cae4cd1392
Gimbalbyte <ethan.morris571@gmail.com>
2021-07-22 20:10:38 -0500

    Added Model data.

---
+++ models/group.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+class Group extends Model {}
+Group.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    group_name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [3],
+      },
+    },
+    type: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "group",
+  }
+);
+module.exports = Group;
