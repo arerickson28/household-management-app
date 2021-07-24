62154f78be5e9d245e19ca8f0344f7ccd9717f0c
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-07-24 13:19:43 -0500

    more javascript work for notepage

--- views/exampleHTML/notepage.js
+++ views/exampleHTML/notepage.js
@@ -1,4 +1,27 @@
 
+
+let list1 = [
+    {
+        itemTitle: "Give Dog A Bath",
+        itemContent: "Remember to check for ticks"
+    },
+    {
+        itemTitle: "Sweep Patio",
+        itemContent: "Remember to compost leaves"
+    }
+]
+
+
+
+
+let newitemTitleInput = document.getElementById("newNoteTitleInput");
+
+let newitemContentInput = document.getElementById("newNoteContentInput");
+
+
+
+
+
 document.getElementById("newNoteBtn").addEventListener("click", function() {
 
     document.getElementById("newNote").classList.remove("hide")
@@ -7,11 +30,26 @@ document.getElementById("newNoteBtn").addEventListener("click", function() {
 
 
 document.getElementById("createNoteBtn").addEventListener("click", function() {
-    document.getElementById("newNote").classList.add("hide")
+    document.getElementById("newNote").classList.add("hide");
+    
+    let newItemObj = {
+        itemTitle: newitemTitleInput.value,
+        itemContent: newitemContentInput.value
+    }
+    
+    
+    list1.push(newItemObj)
+
+    console.log(list1)
+
+    newitemTitleInput.value = ""
+    newitemContentInput.value = ""
 })
 
 document.getElementById("cancelCreateNoteBtn").addEventListener("click", function() {
     document.getElementById("newNote").classList.add("hide")
+    newitemTitleInput.value = ""
+    newitemContentInput.value = ""
 })
 
 console.log("heyooo")
